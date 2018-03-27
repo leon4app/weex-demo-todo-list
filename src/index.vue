830e5a595c7d9195e3b36e1af386ad1e93ded5f9
Leon <Leon4w@outlook.com>
2018-03-27 14:07:06 +0800

    [NEW] Ignore .temp/ [NEW] Delete HelloWorld. [NEW] Add Todo List Table.

--- src/index.vue
+++ src/index.vue
@@ -1,17 +1,13 @@
 <template>
-  <div class="wrapper">
-    <image :src="logo" class="logo" />
-    <text class="greeting">The environment is ready!</text>
-    <HelloWorld/>
-  </div>
+ <TodoList/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList
   },
   data () {
     return {
