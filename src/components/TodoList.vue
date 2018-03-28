7b154af65277d792c3290c9689fcef3f6095d19d
Leon <Leon4w@outlook.com>
2018-03-28 16:06:54 +0800

    [NEW] Delete fixed position codes as it won't scroll in phone os. [NEW] Add input in minibar to add todos.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <wxc-minibar style="position: fixed; z-index: 900"
-                  title="标题"
+    <wxc-minibar  title="标题"
                   background-color="#009ff0"
                   text-color="#FFFFFF"
                   leftButton=""
-                  right-text="Add"
-                  @wxcMinibarLeftButtonClicked="minibarLeftButtonClick"
+                  right-text="+"
                   @wxcMinibarRightButtonClicked="minibarRightButtonClick">
+      <input  slot="middle"
+              class="input"
+              @focus="onFocus"
+              @input="onInput"
+              @change="onChange"
+              placeholder="Add Todo"/>
     </wxc-minibar>
-    <div style="height:90px;"></div> 
-    <list @loadmore="fetch" loadmoreoffset="10">
+    <list @loadmore="fetch" loadmoreoffset="0">
       <cell v-for="(item, i) in todoLists" :key="i" @click="click(item, i)">
         <div class="panel">
           <text class="text">{{item}}</text>
@@ -19,6 +22,37 @@
     </list>
   </div>
 </template>
+
+<style scoped>
+  .input {
+    font-size: 26px;
+    height: 60px;
+    flex: 1;
+    border-radius: 20px;
+    background-color: white;
+  }
+  .panel {
+    /* width: 600px; */
+    flex: 1;
+    /* height: 250px; */
+    margin-left: 30px;
+    margin-right: 30px;
+    margin-top: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    flex-direction: column;
+    justify-content: center;
+    border-width: 2px;
+    border-style: solid;
+    border-color: rgb(162, 217, 192);
+    background-color: rgba(162, 217, 192, 0.2);
+  }
+  .text {
+    font-size: 30px;
+    text-align: center;
+    color: #41B883;
+  }
+</style>
 
 <script>
 import { WxcMinibar } from 'weex-ui'
@@ -29,19 +63,19 @@ export default {
   components: { WxcMinibar },
   data () {
     return {
+      txtInput: '',
       todoLists: [1, 2, 3, 4, 5],
       finishedLists: []
     }
   },
   methods: {
     fetch (event) {
-      modal.toast({ message: 'loadmore', duration: 1 })
-
+      // modal.toast({ message: 'loadmore', duration: 1 })
       setTimeout(() => {
-        const length = this.todoLists.length
-        for (let i = length; i < length + LOADMORE_COUNT; ++i) {
-          this.todoLists.push(i + 1)
-        }
+        // const length = this.todoLists.length
+        // for (let i = length; i < length + LOADMORE_COUNT; ++i) {
+          // this.todoLists.push(i + 1)
+        // }                                                                                          
       }, 800)
     },
     click (item, i) {
@@ -50,37 +84,20 @@ export default {
       this.todoLists.splice(i, 1)
     },
     minibarRightButtonClick () {
-      modal.toast({ 'message': 'click rightButton!', 'duration': 1 })
+      this.todoLists.push(this.txtInput)
+      modal.toast({ 'message': '添加成功!', 'duration': 1 })
+    },
+    onInput (event) {
+      this.txtInput = event.value
+      console.log('oninput', event.value)
+    },
+    onChange (event) {
+      this.txtInput = event.value
+      console.log('onchange', event.value)
+    },
+    onFocus (event) {
+      console.log('onfocus')
     }
   }
 }
 </script>
-
-<style scoped>
-  .header {
-    background-color: aquamarine;
-    border-width: 2px;
-  }
-  .addButton {
-    border-width: 2px;
-    border-color: rgb(162, 217, 192);
-  }
-  .panel {
-    width: 600px;
-    height: 250px;
-    margin-left: 75px;
-    margin-top: 35px;
-    margin-bottom: 35px;
-    flex-direction: column;
-    justify-content: center;
-    border-width: 2px;
-    border-style: solid;
-    border-color: rgb(162, 217, 192);
-    background-color: rgba(162, 217, 192, 0.2);
-  }
-  .text {
-    font-size: 100px;
-    text-align: center;
-    color: #41B883;
-  }
-</style>
